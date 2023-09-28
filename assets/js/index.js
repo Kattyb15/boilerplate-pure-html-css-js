6d51ed8739b318dc578821d0907c5db7e481df40
katherine delgadillo rosas <140263889+Kattyb15@users.noreply.github.com>
2023-09-28 15:35:14 -0600

    cambio

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,51 @@
-let arreglo1 = [
-    "Chilaquiles", 
-    "Tacos", 
-    "Tostadas", 
-    "Flautas",
-    "Enmoladas"
-];
+console.log("Entro index.js");
+
+let inpuntTitulo = document.getElementById("inpuntTitulo");
+let inpuntEstreno = document.getElementById("inpuntEstreno");
+let inpuntGenero = document.getElementById("inpuntGenero");
+let inpuntImagen = document.getElementById("inpuntImagen");
+let inpuntSinopsis = document.getElementById("inpuntSinpsis");
 
 
-console.log(arreglo1[0], "-Posición 0");
-console.log(arreglo1[1], "-Posición 1");
-console.log(arreglo1[2], "-Posición 2");
-console.log(arreglo1[3], "-Posición 3");
-console.log(arreglo1[4], "-Posición 4");
 
-//console.log("La comida del día es: " + arreglo1[3]);
-console.log(`La comida del día es: ${arreglo1[3]}`);
+const btnAgregar = document.getElementById('btnAgregar');
+const btnBorrar = document.getElementById('btnBorrarTodo');
+
+const divPeliculas = document.getElementById("divPeliculas");
+const alertSinPeliculas = document.getElementById("alertSinPeliculas");
 
 
-arreglo1[4] ="Quesadilla";
-console.log(arreglo1[4], "nueva posicion 4");
-console.log("Mutación de arreglo", arreglo1);
+class Pelicula{
+    constructor(titulo, estreno, genero, imagen, sinopsis){
+        this.titulo = titulo;
+        this.estreno = estreno;
+        this.genero = genero;
+        this.imagen = imagen;
+        this.sinopsis = sinopsis;
+    }
+}
 
-//let nombreUsuario = prompt("Cual es tu nombre?");
-//let apellidoUsuario = prompt("Cual es tu apellido?");
+function guardarPelicula(){
+    let titulo = inpuntTitulo.value;
+    let estreno = inpuntEstreno.value;
+    let genero = inpuntGenero.value;
+    let imagen = inpuntImagen.value;
+    let sinopsis = inpuntSinopsis.value;
+    let Pelicula = new Pelicula(
+        titulo,
+        estreno,
+        genero,
+        imagen,
+        sinopsis
+    );
+    console.log(Pelicula);
+    console.log("Entro función guardar pelicular");
+}
 
-//console.log(`El nombre del usuario es: ${nombreUsuario} ${apellidoUsuario}`);
+function borrarTodo(){
+    console.log("Entro a borrar todo");
+}
 
-//arreglo1[5] = nombreUsuario +" "+ apellidoUsuario;
-arreglo1[5] = "burrito";
 
-console.log(arreglo1, "hay un nombre en lugar de comida");
-
-console.log("El largo del arreglo es:", arreglo1.length);
-
-//arreglo1.push("Sopa Tortilla");
-
-//El ultimo fue burrito
-//arreglo1.pop();
-//El ultimo fue quesadilla
-//arreglo1.pop();
-
-//lo manda al principio del arreglo
-//arreglo1.shift();
-let largoarreglo = arreglo1.length;
-let posicionMenosDos = largoarreglo -2;
-console.log(arreglo1[posicionMenosDos]);
-
-//arreglo1.splice(2,1);
-//Primer parametro es el index(posicion) segundo es la cantidad de elemento
-//arreglo1.splice(1,1);
-
-arreglo1.splice(6,1, "Sopes");
-
-console.log(arreglo1);
+btnAgregar.addEventListener("click", guardarPelicula);
+btnBorrarTodo.addEventListener("click", borrarTodo);
